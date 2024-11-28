2b4b47c590e191563d6d27c0b415c888b9511a30
Matthew van der Hoorn <md.registrations@protonmail.com>
2024-11-28 19:53:36 +0100

    Missing argument

--- cli_bench/go/go_cli.py
+++ cli_bench/go/go_cli.py
@@ -7,6 +7,7 @@ from .commands.go_command_context import GoCommandContext
 from .commands.pprof_interactable_command import PprofInteractableCommand
 from .commands.pprof_web_command import PprofWebCommand
 from .commands.standard_benchmark_command import StandardBenchmarkCommand
+from ..common.py_common.user_input.user_input_helper import UserInputHelper
 
 
 class GolangCLI:
@@ -16,12 +17,13 @@ class GolangCLI:
 
 		_file_handler = FileHandler()
 		_command_helper = CommandHelper(self._logger)
+		_user_input_helper = UserInputHelper(self._logger)
 
 		self._commands_to_add = {
 			0: {
 				"keys": ["benchmark", "bm"],
 				"description": "Starts the benchmarking suite.",
-				"command": StandardBenchmarkCommand(self._logger, _file_handler, _command_helper, command_context).run
+				"command": StandardBenchmarkCommand(self._logger, _file_handler, _command_helper, _user_input_helper, command_context).run
 			},
 			1: {
 				"keys": ["benchstat-compare", "bsc"],
